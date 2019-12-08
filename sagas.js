06b557de80cc30144db28423e40867e65107846b
Chris Wray <chriswray76@gmail.com>
2019-12-08 22:10:03 +0000

    completed saga tutorial

---
+++ sagas.js
@@ -0,0 +1,23 @@
+import { put, call, takeEvery, all } from 'redux-saga/effects';
+
+export const delay = (ms) => new Promise(res => setTimeout(res, ms));
+
+function* helloSaga() {
+  console.log('Hello Sagas!');
+}
+
+export function* incrementAsync() {
+  yield call(delay, 1000);
+  yield put({ type: 'INCREMENT' });
+}
+
+function* watchIncrementAsync() {
+  yield takeEvery('INCREMENT_ASYNC', incrementAsync);
+}
+
+export function* rootSaga() {
+  yield all([
+    helloSaga(),
+    watchIncrementAsync()
+  ]);
+}
